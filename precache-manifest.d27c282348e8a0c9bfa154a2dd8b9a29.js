ca32e87ec30c04960a9435cdb3345e106ab1fa80
Woo Song <ws392@drexel.edu>
2019-05-14 19:05:18 -0400

    Updates

---
+++ precache-manifest.d27c282348e8a0c9bfa154a2dd8b9a29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "228cc96e4777b95dac22",
+    "url": "/pankeeki/static/js/runtime~main.7157be5d.js"
+  },
+  {
+    "revision": "34ed36828901399bd350",
+    "url": "/pankeeki/static/js/main.ed78481f.chunk.js"
+  },
+  {
+    "revision": "59cf104baed73037a79b",
+    "url": "/pankeeki/static/js/2.db758d1b.chunk.js"
+  },
+  {
+    "revision": "34ed36828901399bd350",
+    "url": "/pankeeki/static/css/main.b1c6c51b.chunk.css"
+  },
+  {
+    "revision": "46e86d92fbb0b6eaf913c0a21495c1eb",
+    "url": "/pankeeki/index.html"
+  }
+];
